773efd17bc0452aa6cb2c4af52b4771a2c3a2630
Kathleen Kamali <kathleenkamali@gmail.com>
2020-01-22 13:53:05 -0800

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  const time_parts = time.split(":");
+  const hour = parseInt(time_parts[0]);
+  if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon";
+  } else if (hour >= 17) {
+    return "Good Evening";
+  } else {
+    return "Good Morning";
+  }
+}
+
+function displayMessage(message) {
+  const greeting = document.getElementById('greeting');
+  greeting.innerText = message;
+}

a5eef535c5307dd62bd7585978e138461e26e1cc
Gina Sekhar <ginasekhar@yahoo.com>
2020-01-24 09:59:07 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time_str) {
+  const input_time = time_str.split(':');
+  const input_hour = parseInt(input_time[0]);
+  
+  if (input_hour < 12) 
+  {
+    return "Good Morning"
+  }
+  else if ((input_hour >= 12) && (input_hour <= 17))
+  {
+    return "Good Afternoon"
+  }
+  else
+  {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  
+  let gr = document.getElementById("greeting");
+  gr.innerText = msg
+}

b2fdcbd542d941c687cc4cb05c396571042030f3
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-01-26 19:22:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const splitTime = time.split(":");
+  const hour = parseInt(splitTime[0], 10);
+
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}

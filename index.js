438c49f74a693b65425ccbb848ca0b685429521e
github username <github email address>
2020-02-20 13:03:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
-}
-
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-function greet(timeString) {
-  const hour = parseInt(timeString.split(":")[0]);
-  if (hour < 12) {
-    return "Good Morning";
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
   }
-  if (hour >= 12 && hour <= 17) {
-    return "Good Afternoon";
-  } else {
-    return "Good Evening";
-  }
-}
-/* Write your implementation of displayMessage() */
+  
+  /* End Given Code, don't edit above here...*/
+  
+  /* Write your implementation of greet() */
 
-function displayMessage(message) {
-  const greeting = document.querySelector("h1#greeting")
-  greeting.innerText = message
-}
+  function greet(timeString) {
+      const hour = parseInt(timeString.split(":")[0])
+      if (hour < 12) {
+          return "Good Morning"
+      }
+      if (hour >= 12 && hour <= 17) {
+          return "Good Afternoon"
+      }
+      if (hour > 17) {
+          return "Good Evening"
+      }
+  }
+  
+  /* Write your implementation of displayMessage() */
+
+  function displayMessage(message) {
+      const greeting = document.querySelector('h1#greeting')
+      greeting.innerText = message
+  }
